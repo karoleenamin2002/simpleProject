557877931dab8e5304252b2c83dd3a3b07cf0461
Karoleen Amin <karolamin47@gmail.com>
2024-07-04 23:00:23 +0300

    Stack and Positioned

--- lib/main.dart
+++ lib/main.dart
@@ -23,156 +23,105 @@ class Simpleproject extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: Text(
-          "Facebook",
-          style: TextStyle(
-              fontSize: 30, fontWeight: FontWeight.bold, color: Colors.blue),
-        ),
-        centerTitle: true,
-        leading: IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.menu, size: 33, color: Colors.blue)),
-        actions: [
-          IconButton(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          title: Text(
+            "Facebook",
+            style: TextStyle(
+                fontSize: 30, fontWeight: FontWeight.bold, color: Colors.blue),
+          ),
+          centerTitle: true,
+          leading: IconButton(
               onPressed: () {},
-              icon: Icon(
-                Icons.message,
-                size: 33,
-                color: Colors.blue,
-              )),
-          IconButton(
-              onPressed: () {},
-              icon: Icon(
-                Icons.search,
-                size: 33,
-                color: Colors.blue,
-              ))
-        ],
-        elevation: 10,
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(Icons.add),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              margin: EdgeInsets.only(bottom: 30),
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  color: Colors.blueGrey),
-              width: double.infinity,
-              height: 400,
-              child: Text(
-                "Karoleen Ameen",
-                style: TextStyle(color: Colors.white, fontSize: 30),
-              ),
-            ),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children: [
-                  Container(
-                    margin: EdgeInsets.fromLTRB(0, 10, 30, 30),
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: Colors.blueGrey),
-                    width: 200,
-                    height: 100,
-                    child: Text(
-                      "Karo",
-                      style: TextStyle(color: Colors.white, fontSize: 30),
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.fromLTRB(0, 10, 30, 30),
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: Colors.blueGrey),
-                    width: 200,
-                    height: 100,
-                    child: Text(
-                      "Karol",
-                      style: TextStyle(color: Colors.white, fontSize: 30),
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.fromLTRB(0, 10, 30, 30),
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: Colors.blueGrey),
-                    width: 200,
-                    height: 100,
-                    child: Text(
-                      "Koka",
-                      style: TextStyle(color: Colors.white, fontSize: 30),
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.fromLTRB(0, 10, 30, 30),
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: Colors.blueGrey),
-                    width: 200,
-                    height: 100,
-                    child: Text(
-                      "Koko",
-                      style: TextStyle(color: Colors.white, fontSize: 30),
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.fromLTRB(0, 10, 30, 30),
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: Colors.blueGrey),
-                    width: 200,
-                    height: 100,
-                    child: Text(
-                      "Karol",
-                      style: TextStyle(color: Colors.white, fontSize: 30),
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.fromLTRB(0, 10, 30, 30),
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: Colors.blueGrey),
-                    width: 200,
-                    height: 100,
-                    child: Text(
-                      "Krkr",
-                      style: TextStyle(color: Colors.white, fontSize: 30),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.only(bottom: 30),
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  color: Colors.blueGrey),
-              width: double.infinity,
-              height: 400,
-              child: Text(
-                "Karoleen Ameen",
-                style: TextStyle(color: Colors.white, fontSize: 30),
-              ),
-            ),
+              icon: Icon(Icons.menu, size: 33, color: Colors.blue)),
+          actions: [
+            IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.message,
+                  size: 33,
+                  color: Colors.blue,
+                )),
+            IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.search,
+                  size: 33,
+                  color: Colors.blue,
+                ))
           ],
+          elevation: 10,
         ),
-      ),
-    );
+        body: Center(
+            child: Container(
+          padding: EdgeInsets.all(10),
+          width: 300,
+          height: 300,
+          color: Colors.blueGrey,
+          child: Stack(children: [
+            Positioned(
+              width: 100,
+              height: 100,
+              top: 0,
+              left: 0,
+              child: Container(
+                alignment: Alignment.center,
+                color: Colors.green[200],
+                child: Text(
+                  "C4a.Shop",
+                  style: TextStyle(fontSize: 22),
+                ),
+              ),
+            ),
+            Positioned(
+              width: 100,
+              height: 100,
+              top: 0,
+              right: 0,
+              child: Container(
+                alignment: Alignment.center,
+                color: Colors.amber[200],
+                child: Text(
+                  "C4a.Shop",
+                  style: TextStyle(fontSize: 22),
+                ),
+              ),
+            ),
+            Positioned(
+              width: 100,
+              height: 100,
+              bottom: 0,
+              right: 0,
+              child: Container(
+                alignment: Alignment.center,
+                color: Colors.blue[300],
+                child: Text(
+                  "C4a.Shop",
+                  style: TextStyle(fontSize: 22,color: Colors.white),
+                ),
+              ),
+            ),
+            Positioned(
+              width: 100,
+              height: 100,
+              bottom: 0,
+              left: 0,
+              child: Container(
+                alignment: Alignment.center,
+                color: Colors.pink[300],
+                child: Text(
+                  "C4a.Shop",
+                  style: TextStyle(fontSize: 22,color: Colors.white),
+                ),
+              ),
+            ),
+            Center(
+              child: Container(alignment: Alignment.center,width: 100,height: 100,color: Colors.red[300],child:Text(
+                    "C4a.Shop",
+                    style: TextStyle(fontSize: 22,color: Colors.white),
+                  ) ,),
+            )
+          ]),
+        )));
   }
 }
