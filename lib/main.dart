a2a489bda008891b03997a6512aa469014df86b4
Karoleen Amin <karolamin47@gmail.com>
2024-07-04 22:08:48 +0300

    add Column and Scroll

--- lib/main.dart
+++ lib/main.dart
@@ -23,66 +23,115 @@ class Simpleproject extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-            onPressed: () {},
-            icon: Icon(
-              Icons.menu,
-              size: 30,
-            )),
-        title: Text("Facebook"),
-        centerTitle: true,
-        actions: [
-          IconButton(
+        appBar: AppBar(
+          leading: IconButton(
               onPressed: () {},
               icon: Icon(
-                Icons.message,
-                size: 25,
+                Icons.menu,
+                size: 30,
               )),
-          IconButton(
-              onPressed: () {},
-              icon: Icon(
-                Icons.search,
-                size: 25,
-              ))
-        ],
-        elevation: 10,
-      ),
-      floatingActionButton:
-          FloatingActionButton(onPressed: () {}, child: Icon(Icons.add)),
-      body: Container(
-        width: 300,
-        height: 300,
-      //  transform: Matrix4.rotationZ(0.2),
-        padding: EdgeInsets.all(11),
-        margin: EdgeInsets.all(11),
-        alignment: Alignment.center,
-        decoration: BoxDecoration(
-            color: Colors.greenAccent,
-            border: Border.all(
-              color: Colors.green,width: 15
-            ),
-          //  borderRadius: BorderRadius.circular(30),
-            shape: BoxShape.circle
-           ),
-        child: Text(
-          "Four Cats..",
-          style: TextStyle(
-              fontSize: 40,
-            //  decoration: TextDecoration.underline,
-              fontStyle: FontStyle.italic,
-              fontWeight: FontWeight.w500,
-              color: Colors.blue,
-            //  backgroundColor: Colors.white,
-              height: 2.5,
-              wordSpacing: 3,
-              letterSpacing: 1.5),
-          maxLines: 2,
-          overflow: TextOverflow.ellipsis,
-          textAlign: TextAlign.center,
-          textDirection: TextDirection.rtl,
+          title: Text("Facebook"),
+          centerTitle: true,
+          actions: [
+            IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.message,
+                  size: 25,
+                )),
+            IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.search,
+                  size: 25,
+                ))
+          ],
+          elevation: 10,
         ),
-      ),
-    );
+        floatingActionButton:
+            FloatingActionButton(onPressed: () {}, child: Icon(Icons.add)),
+        body: SingleChildScrollView(
+          padding: EdgeInsets.all(10),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  width: 300,
+                  height: 300,
+                  padding: EdgeInsets.all(11),
+                  margin: EdgeInsets.all(11),
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      color: Colors.greenAccent,
+                      border: Border.all(color: Colors.green, width: 15),
+                      shape: BoxShape.circle),
+                  child: Text(
+                    "Four Cats..",
+                    style: TextStyle(
+                      fontSize: 40,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.black,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                SizedBox(height: 25,),
+                Text(
+                  "Karoo",
+                  style: TextStyle(backgroundColor: Colors.grey,
+                      fontSize: 35,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black),
+                ),
+                                SizedBox(height: 25,),
+
+                IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.favorite,
+                      size: 80,
+                      color: Colors.red,
+                    )),                SizedBox(height: 35,),
+
+                    Container(
+                  width: 300,
+                  height: 300,
+                  padding: EdgeInsets.all(11),
+                  margin: EdgeInsets.all(11),
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      color: Colors.greenAccent,
+                      border: Border.all(color: Colors.green, width: 15),
+                      shape: BoxShape.circle),
+                  child: Text(
+                    "Four Cats..",
+                    style: TextStyle(
+                      fontSize: 40,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.black,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                SizedBox(height: 25,),
+                Text(
+                  "Karoo",
+                  style: TextStyle(
+                      fontSize: 35,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black),
+                ),
+                                SizedBox(height: 25,),
+
+                IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.favorite,
+                      size: 80,
+                      color: Colors.red,
+                    ))
+              ]),
+        ));
   }
 }
