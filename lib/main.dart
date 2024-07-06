7d89f2b2b7ebe4f3894d163855edb8d3711d1c28
Karoleen Amin <karolamin47@gmail.com>
2024-07-06 21:35:42 +0300

    elevatedbutton&wrap

--- lib/main.dart
+++ lib/main.dart
@@ -23,105 +23,141 @@ class Simpleproject extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          title: Text(
-            "Facebook",
-            style: TextStyle(
-                fontSize: 30, fontWeight: FontWeight.bold, color: Colors.blue),
-          ),
-          centerTitle: true,
-          leading: IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.menu, size: 33, color: Colors.blue)),
-          actions: [
-            IconButton(
-                onPressed: () {},
-                icon: Icon(
-                  Icons.message,
-                  size: 33,
-                  color: Colors.blue,
-                )),
-            IconButton(
-                onPressed: () {},
-                icon: Icon(
-                  Icons.search,
-                  size: 33,
-                  color: Colors.blue,
-                ))
-          ],
-          elevation: 10,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text(
+          "Facebook",
+          style: TextStyle(
+              fontSize: 30, fontWeight: FontWeight.bold, color: Colors.blue),
         ),
-        body: Center(
-            child: Container(
-          padding: EdgeInsets.all(10),
-          width: 300,
-          height: 300,
-          color: Colors.blueGrey,
-          child: Stack(children: [
-            Positioned(
-              width: 100,
-              height: 100,
-              top: 0,
-              left: 0,
-              child: Container(
-                alignment: Alignment.center,
-                color: Colors.green[200],
-                child: Text(
-                  "C4a.Shop",
-                  style: TextStyle(fontSize: 22),
+        centerTitle: true,
+        leading: IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.menu, size: 33, color: Colors.blue)),
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.message,
+                size: 33,
+                color: Colors.blue,
+              )),
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.search,
+                size: 33,
+                color: Colors.blue,
+              ))
+        ],
+        elevation: 10,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.only(bottom: 30),
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.blueGrey),
+              width: double.infinity,
+              height: 400,
+              child: Text(
+                "Karoleen Ameen",
+                style: TextStyle(color: Colors.white, fontSize: 30),
+              ),
+            ),
+            Container(color: Colors.blue[200],width: 250,height: 200,margin: EdgeInsets.all(15),padding: EdgeInsets.all(6),
+              child: Center(
+                child: Wrap(alignment: WrapAlignment.end,direction: Axis.vertical,
+                spacing: 40,
+                runSpacing: 30,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Text("click",style: TextStyle(fontSize: 30),),
+                      style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStatePropertyAll(Colors.amber[300]),
+                              foregroundColor: MaterialStatePropertyAll(Colors.blue[200]),
+                              padding: MaterialStatePropertyAll(EdgeInsets.all(7)),
+                              shape: MaterialStatePropertyAll(RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)))),
+                    ),
+                      ElevatedButton(
+                      onPressed: () {},
+                      child: Text("click",style: TextStyle(fontSize: 30)),
+                      style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStatePropertyAll(Colors.amber[300]),
+                              foregroundColor: MaterialStatePropertyAll(Colors.blue[200]),
+                              padding: MaterialStatePropertyAll(EdgeInsets.all(7)),
+                              shape: MaterialStatePropertyAll(RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)))),
+                    ),
+                      ElevatedButton(
+                      onPressed: () {},
+                      child:Text("click",style: TextStyle(fontSize: 30)),
+                      style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStatePropertyAll(Colors.amber[300]),
+                              foregroundColor: MaterialStatePropertyAll(Colors.blue[200]),
+                              padding: MaterialStatePropertyAll(EdgeInsets.all(7)),
+                              shape: MaterialStatePropertyAll(RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)))),
+                    ),
+                      ElevatedButton(
+                      onPressed: () {},
+                      child: Text("click",style: TextStyle(fontSize: 30)),
+                      style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStatePropertyAll(Colors.amber[300]),
+                              foregroundColor: MaterialStatePropertyAll(Colors.blue[200]),
+                              padding: MaterialStatePropertyAll(EdgeInsets.all(7)),
+                              shape: MaterialStatePropertyAll(RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)))),
+                    ),
+                      ElevatedButton(
+                      onPressed: () {},
+                      child: Text("click",style: TextStyle(fontSize: 30)),
+                      style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStatePropertyAll(Colors.amber[300]),
+                              foregroundColor: MaterialStatePropertyAll(Colors.blue[200]),
+                              padding: MaterialStatePropertyAll(EdgeInsets.all(7)),
+                              shape: MaterialStatePropertyAll(RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)))),
+                    ),
+                      ElevatedButton(
+                      onPressed: () {},
+                      child: Text("click",style: TextStyle(fontSize: 30)),
+                      style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStatePropertyAll(Colors.amber[300]),
+                              foregroundColor: MaterialStatePropertyAll(Colors.blue[200]),
+                              padding: MaterialStatePropertyAll(EdgeInsets.all(7)),
+                              shape: MaterialStatePropertyAll(RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)))),
+                    ),
+                  ],
                 ),
               ),
             ),
-            Positioned(
-              width: 100,
-              height: 100,
-              top: 0,
-              right: 0,
-              child: Container(
-                alignment: Alignment.center,
-                color: Colors.amber[200],
-                child: Text(
-                  "C4a.Shop",
-                  style: TextStyle(fontSize: 22),
-                ),
+            Container(
+              margin: EdgeInsets.only(bottom: 30),
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.blueGrey),
+              width: double.infinity,
+              height: 400,
+              child: Text(
+                "Karoleen Ameen",
+                style: TextStyle(color: Colors.white, fontSize: 30),
               ),
             ),
-            Positioned(
-              width: 100,
-              height: 100,
-              bottom: 0,
-              right: 0,
-              child: Container(
-                alignment: Alignment.center,
-                color: Colors.blue[300],
-                child: Text(
-                  "C4a.Shop",
-                  style: TextStyle(fontSize: 22,color: Colors.white),
-                ),
-              ),
-            ),
-            Positioned(
-              width: 100,
-              height: 100,
-              bottom: 0,
-              left: 0,
-              child: Container(
-                alignment: Alignment.center,
-                color: Colors.pink[300],
-                child: Text(
-                  "C4a.Shop",
-                  style: TextStyle(fontSize: 22,color: Colors.white),
-                ),
-              ),
-            ),
-            Center(
-              child: Container(alignment: Alignment.center,width: 100,height: 100,color: Colors.red[300],child:Text(
-                    "C4a.Shop",
-                    style: TextStyle(fontSize: 22,color: Colors.white),
-                  ) ,),
-            )
-          ]),
-        )));
+          ],
+        ),
+      ),
+    );
   }
 }
